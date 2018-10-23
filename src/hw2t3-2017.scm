adf5abae395466221ba863638f5083998ddfaa4e
NoHomey <ivo.stratev.tues@gmail.com>
2018-10-23 23:02:43 +0300

    adding basic implementation of calculating derivative

--- src/hw2t3-2017.scm
+++ src/hw2t3-2017.scm
@@ -12,11 +12,86 @@
 )
 
 (define (eval-tree t x)
-    ((lambda (f t) (f f t)) (lambda (self t)
+    (cond
+        ((eq? 'x t) x)
+        ((number? t) t)
+        (else ((symbol->func (car t)) (eval-tree (car (cdr t))) (eval-tree (car (cdr (cdr t))))))
+    )
+)
+
+(define (tree-simplify? t)
+    (split-tree (lambda (s f g)
         (cond
-            ((eq? 'x t) x)
-            ((number? t) t)
-            (else ((symbol->func (car t)) (self self (car (cdr t))) (self self (car (cdr (cdr t))))))
+            ((or (eq? '+ s) (eq? '- s))
+                (or
+                    (and (number? f) (number? g))
+                    (and (number? f) (= 0 f))
+                    (and (number? g) (= 0 g))
+                )
+            )
+            ((eq? '* s)
+                (or
+                    (and (number? f) (number? g))
+                    (and (number? f) (or (= 0 f) (= 1 f)))
+                    (and (number? g) (or (= 0 g) (= 1 g)))
+                )
+            )
+            (else #f)
         )
     ) t)
+)
+
+(define (keep-tree-simplify t) (if (tree-simplify? t) (tree-simplify t) t))
+
+(define (split-tree f t) (f (car t) (car (cdr t)) (car (cdr (cdr t)))))
+
+(define (tree-simplify t)
+    (if (list? t)
+        (split-tree (lambda (s f g)
+            (cond
+                ((or (eq? '+ s) (eq? '- s))
+                    (cond
+                        ((and (number? f) (number? g) ((if (eq? '+ s) + -) f g)))
+                        ((and (number? f) (= 0 f)) (tree-simplify g))
+                        ((and (number? g) (= 0 g)) (tree-simplify f))
+                        (else (keep-tree-simplify (list s (tree-simplify f) (tree-simplify g))))
+                    )
+                )
+                ((eq? '* s) (cond
+                    ((and (number? f) (number? g) (* f g)))
+                    ((number? f)
+                        (cond
+                            ((= 0 f) 0)
+                            ((= 1 f) (tree-simplify g))
+                            (else (keep-tree-simplify (list s f (tree-simplify g))))
+                        )
+                    )
+                    ((number? g)
+                        (cond
+                            ((= 0 g) 0)
+                            ((= 1 g) (tree-simplify f))
+                            (else (keep-tree-simplify (list s (tree-simplify f) g)))
+                        )
+                    )
+                    (else (keep-tree-simplify (list s (tree-simplify f) (tree-simplify g))))
+                ))
+                (else t)
+            )
+        ) t)
+    )
+)
+
+(define (tree-derive t)
+    (cond
+        ((eq? 'x t) 1)
+        ((number? t) 0)
+        (else (split-tree (lambda (s f g)
+            (cond
+                ((or (eq? '+ s) (eq? '- s)) (list s (tree-derive f) (tree-derive g)))
+                ((eq? '* s) (list '+ (list '* (tree-derive f) g) (list '* f (tree-derive g))))
+                ((eq? '/ s) (list '/ (list '+ (list '* (tree-derive f) g) (list '* f (tree-derive g))) (list '* g g)))
+                (else #f)
+            )
+        ) t))
+    )
 )
